d91a6d0c7ff1d3fa04f51c570934d6d61437175a
diego <diegozaratean@gmail.com>
2025-08-29 19:49:17 +0000

    finish login signup logout

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,6 +1,20 @@
 import { Link } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+	const { store, dispatch } = useGlobalReducer()
+
+	const navigate = useNavigate()
+
+	function logout(){
+		// limpiar token
+		localStorage.removeItem("token");
+		// pasar auth a false
+		dispatch({ type: "set_auth", payload: false })
+		// redirecionar a home
+		navigate('/')
+	}
 
 	return (
 		<nav className="navbar navbar-light bg-light">
@@ -8,6 +22,19 @@ export const Navbar = () => {
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
+				{ store.auth ? <button className="btn btn-primary" onClick={logout}>logout</button> : null }
+
+				{ store.auth ? 
+					<Link to="/">
+						<button className="btn btn-primary" onClick={()=>{
+							// limpiar token
+							localStorage.removeItem("token");
+							// pasar auth a false
+							dispatch({ type: "set_auth", payload: false })
+							// redirecionar a home
+						}}>logout 2</button>
+					</Link>
+				: null }
 				<div className="ml-auto">
 					<Link to="/demo">
 						<button className="btn btn-primary">Check the Context in action</button>
